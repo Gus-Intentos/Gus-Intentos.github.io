346cf2b40be9de3eab6c74dfb96adab3331b87d2
Gus-Intentos <114002981+Gus-Intentos@users.noreply.github.com>
2022-10-03 18:46:00 +0200

    Add files via upload

---
+++ Java/Actividad 9/js/app.js
@@ -0,0 +1,15 @@
+$('#slider').owlCarousel({
+    loop: true,
+    autoplay: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items :1
+        },
+        1000: {
+            items: 1
+        }
+    }
+})
